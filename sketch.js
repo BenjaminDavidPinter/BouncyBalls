ffd7ff31f27075f00647df4c273755d71b969c4a
BenjaminDavidPinter <72715672+BenjaminDavidPinter@users.noreply.github.com>
2021-08-24 23:47:36 -0400

    Added colored lines to help understand the movements of each ball

--- sketch.js
+++ sketch.js
@@ -1,48 +1,74 @@
 //Balls config
 
-Total_Balls = 100;
+Total_Balls = 1;
 Balls = [];
 
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  frameRate(60);
-  for(let i = 0; i < Total_Balls; i++){
-    Balls[i] = {
-      Color: {
-        R: getRandomInt(100, 255),
-        G: getRandomInt(100, 255),
-        B: getRandomInt(100, 255)
-      },
-      Size: getRandomInt(5,15),
-      Vec: createVector(windowWidth/2, windowHeight/2),
-      XSpeed: getRandomInt(-10, 10),
-      YSpeed: getRandomInt(-10, 10)
-    }
-  }
+	createCanvas(windowWidth, windowHeight);
+	frameRate(60);
+	for(let i = 0; i < Total_Balls; i++){
+		Balls[i] = {
+			Color: {
+				R: getRandomInt(100, 255),
+				G: getRandomInt(100, 255),
+				B: getRandomInt(100, 255)
+			},
+			Size: getRandomInt(5,15),
+			Vec: createVector(windowWidth/2, windowHeight/2),
+			XSpeed: getRandomInt(-10, 10),
+			YSpeed: getRandomInt(-10, 10),
+			PreviousPositions: []
+		}
+	}
 }
 
 function draw() {
-  clear();
-  for(let i = 0; i < Total_Balls; i++){
-    fill(Balls[i].Color.R, Balls[i].Color.G, Balls[i].Color.B);
-    circle(Balls[i].Vec.x, Balls[i].Vec.y, Balls[i].Size);
-    Balls[i].Vec.x = Balls[i].Vec.x + Balls[i].XSpeed;
-    Balls[i].Vec.y = Balls[i].Vec.y + Balls[i].YSpeed;
+	clear();
+	for(let i = 0; i < Total_Balls; i++){
 
-    //Collision with walls
-    if(Balls[i].Vec.x <= 0 + Balls[i].Size/2
-    || Balls[i].Vec.x >= windowWidth - Balls[i].Size/2){
-        Balls[i].XSpeed = Balls[i].XSpeed * -1;
-    }
+		//Move
+		let positionLog = {
+			previousX : Balls[i].Vec.x,
+			previousY : Balls[i].Vec.y
+		};
+		Balls[i].Vec.x = Balls[i].Vec.x + Balls[i].XSpeed;
+		Balls[i].Vec.y = Balls[i].Vec.y + Balls[i].YSpeed;
 
-    if(Balls[i].Vec.y >= windowHeight - Balls[i].Size/2
-    || Balls[i].Vec.y <= 0 + Balls[i].Size/2){
-        Balls[i].YSpeed = Balls[i].YSpeed * -1
-    }
-    //End Wall Collision
-  }
+		positionLog.currentX = Balls[i].Vec.x;
+		positionLog.currentY = Balls[i].Vec.y;
+
+		Balls[i].PreviousPositions.push(positionLog);
+
+		for(let j = 0; j < Balls[i].PreviousPositions.length; j++){
+			if(Balls[i].PreviousPositions[j].previousY > Balls[i].PreviousPositions[j].currentY){
+				stroke(0,255,0);
+			} else {
+				stroke(255,0,0);
+			}
+			line(Balls[i].PreviousPositions[j].previousX,
+				Balls[i].PreviousPositions[j].previousY,
+				Balls[i].PreviousPositions[j].currentX,
+				Balls[i].PreviousPositions[j].currentY);
+		}
+
+		//Collision with walls
+		if(Balls[i].Vec.x <= 0 + Balls[i].Size/2
+			|| Balls[i].Vec.x >= windowWidth - Balls[i].Size/2){
+			Balls[i].XSpeed = Balls[i].XSpeed * -1;
+		}
+
+		if(Balls[i].Vec.y >= windowHeight - Balls[i].Size/2
+			|| Balls[i].Vec.y <= 0 + Balls[i].Size/2){
+			Balls[i].YSpeed = Balls[i].YSpeed * -1
+		}
+		//End Wall Collision
+		
+		fill(Balls[i].Color.R, Balls[i].Color.G, Balls[i].Color.B);
+		circle(Balls[i].Vec.x, Balls[i].Vec.y, Balls[i].Size);
+	}
 }
 
 function getRandomInt(min, max) {
-  return Math.random() * (max - min) + min;
+	return Math.random() * (max - min) + min;
 }
